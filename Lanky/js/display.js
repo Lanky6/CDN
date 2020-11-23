b38c4478507f7e21afb4e1f319acc56504a7d6c3
DK01689 <Lin0051121@gmail.com>
2020-11-23 21:04:45 +0800

    Update display.js

--- Lanky/js/display.js
+++ Lanky/js/display.js
@@ -19,3 +19,14 @@ divs[i].style.display="none"
 }
 }
 }
+
+function show(id){
+var divs = document.getElementsByName("menu")
+for (var i = 0 ; i < divs.length ; i++){
+if (divs[i].id == "m"+id ){
+divs[i].style.display=""
+}else{
+divs[i].style.display="none"
+}
+}
+}
