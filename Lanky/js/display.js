f062360903a06caed2b8b27f32c843ad1b01ef3d
DK01689 <Lin0051121@gmail.com>
2020-11-23 21:37:54 +0800

    Update display.js

--- Lanky/js/display.js
+++ Lanky/js/display.js
@@ -1,29 +1,7 @@
 function show(id){
-var divs = document.getElementsByName("box")
-for (var i = 0 ; i < divs.length ; i++){
-if (divs[i].id == "b"+id ){
-divs[i].style.display=""
-}else{
-divs[i].style.display="none"
-}
-}
-}
-
-function show(id){
 var divs = document.getElementsByName("nav")
 for (var i = 0 ; i < divs.length ; i++){
 if (divs[i].id == "n"+id ){
-divs[i].style.display=""
-}else{
-divs[i].style.display="none"
-}
-}
-}
-
-function show(id){
-var divs = document.getElementsByName("menu")
-for (var i = 0 ; i < divs.length ; i++){
-if (divs[i].id == "m"+id ){
 divs[i].style.display=""
 }else{
 divs[i].style.display="none"
